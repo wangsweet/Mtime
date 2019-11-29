4195110b68651852432dbac4cdd5747ca986dd94
BK1917wangshuang <2667415248@qq.com>
2019-11-29 21:39:52 +0800

    router

--- src/router/index.js
+++ src/router/index.js
@@ -1,164 +1,168 @@
-import {Home,Shop,Find,Mine,Cart,Goodslist,Goodsdetail,Registe,Login,ActorDetail,ActorList,CinemaDetail,City,MovieDetail,MovieShow,Search,Ticket,MovieShowing,MovieComing} from "../pages"
+import { Home, Shop, Find, Mine, Cart, Goodslist, Goodsdetail, Registe, Login, ActorDetail, ActorList, CinemaDetail, City, MovieDetail, MovieShow, Search, Ticket, MovieShowing, MovieComing } from "../pages"
 
 //用来配置带head的路由
-export const HeadRoutes=[
+export const HeadRoutes = [
     {
-        path:"/home",
-        component:Home,
-        meta:{
-            flag:true
+        path: "/home",
+        component: Home,
+        meta: {
+            flag: true
         },
-        text:"首页"
+        text: "首页"
     },
     {
-        path:"/ticket",
-        component:Ticket,
-        meta:{
-            flag:true,
-            requiredAuth:false,
+        path: "/ticket",
+        component: Ticket,
+        meta: {
+            flag: true,
+            requiredAuth: false,
         },
-        text:"购票"
+        text: "购票"
     },
     {
-        path:"/shop",
-        component:Shop,
-        meta:{
-            flag:true
+        path: "/shop",
+        component: Shop,
+        meta: {
+            flag: true
         },
-        text:"商城"
+        text: "商城"
     },
     {
-        path:"/find",
-        component:Find,
-        meta:{
-            flag:true
+        path: "/find",
+        component: Find,
+        meta: {
+            flag: true
         },
-        text:"发现"
+        text: "发现"
     },
-   
+
 ]
 
 //用来配置不带head的路由
 
-export const NoHeadRoutes=[
+export const NoHeadRoutes = [
     {
-        path:"/mine",
-        component:Mine,
-        meta:{
-            flag:false,
-            requiredAuth:false,
+        path: "/mine",
+        component: Mine,
+        meta: {
+            flag: false,
+            requiredAuth: false,
         }
     },
     {
-        path:"/login",
-        component:Login,
-        meta:{
-            flag:false
+        path: "/login",
+        component: Login,
+        meta: {
+            flag: false
         }
     },
     {
-        path:"/registe",
-        component:Registe,
-        meta:{
-            flag:false,
-            requiredAuth:false
+        path: "/registe",
+        component: Registe,
+        meta: {
+            flag: false,
+            requiredAuth: false
         }
     },
     {
-        path:"/cart",
-        component:Cart,
-        meta:{
-            flag:false,
-            requiredAuth:false
+        path: "/cart",
+        component: Cart,
+        meta: {
+            flag: false,
+            requiredAuth: false
         }
 
     },
     {
-        path:"/goodslist",
-        component:Goodslist,
-        meta:{
-            flag:false,
-            requiredAuth:false
+        path: "/goodslist",
+        component: Goodslist,
+        meta: {
+            flag: false,
+            requiredAuth: false
         }
     },
     {
-        path:"/goodsdetail",
-        component:Goodsdetail,
-        meta:{
-            flag:false,
-            requiredAuth:false
+        path: "/goodsdetail",
+        component: Goodsdetail,
+        meta: {
+            flag: false,
+            requiredAuth: false
         }
     },
     {
-        path:"/actorDetail",
-        component:ActorDetail,
-        meta:{
-            flag:false,
-            requiredAuth:false
+        path: "/actorDetail",
+        component: ActorDetail,
+        meta: {
+            flag: false,
+            requiredAuth: false
         }
     },
     {
-        path:"/actorList",
-        component:ActorDetail,
-        meta:{
-            flag:false,
-            requiredAuth:false
+        path: "/actorList",
+        component: ActorList,
+        meta: {
+            flag: false,
+            requiredAuth: false
         }
     },
     {
-        path:"/cinemaDetail",
-        component:CinemaDetail,
-        meta:{
-            flag:false,
-            requiredAuth:false
+        path: "/cinemaDetail",
+        component: CinemaDetail,
+        meta: {
+            flag: false,
+            requiredAuth: false
         }
     },
     {
-        path:"/city",
-        component:City,
-        meta:{
-            flag:false,
-            requiredAuth:false
+        path: "/city",
+        component: City,
+        meta: {
+            flag: false,
+            requiredAuth: false
         }
     },
     {
-        path:"/movieDetail",
-        component:MovieDetail,
-        meta:{
-            flag:false,
-            requiredAuth:false
+        path: "/movieDetail",
+        component: MovieDetail,
+        meta: {
+            flag: false,
+            requiredAuth: false
         }
     },
     {
-        path:"/movieShow",
-        component:MovieShow,
-        meta:{
-            flag:false,
-            requiredAuth:false
+        path: "/movieShow",
+        component: MovieShow,
+        meta: {
+            flag: false,
+            requiredAuth: false
         },
-        children:{
-            path:"/movieShowing",
-            component:MovieShowing,
-            meta:{
-                flag:false,
-                requiredAuth:false
+        children: [
+            {
+                path: "/movieShowing",
+                component: MovieShowing,
+                meta: {
+                    flag: false,
+                    requiredAuth: false
+                }
             },
-            path:"/movieComing",
-            component:MovieComing,
-            meta:{
-                flag:false,
-                requiredAuth:false
+            {
+                path: "/movieComing",
+                component: MovieComing,
+                meta: {
+                    flag: false,
+                    requiredAuth: false
+                }
             },
-        }
+        ]
     },
     {
-        path:"/search",
-        component:Search,
-        meta:{
-            flag:false,
-            requiredAuth:false
+        path: "/search",
+        component: Search,
+        meta: {
+            flag: false,
+            requiredAuth: false
         }
     },
 ]
 
-export const RouteConfig= HeadRoutes.concat(NoHeadRoutes);
+export const RouteConfig = HeadRoutes.concat(NoHeadRoutes);
